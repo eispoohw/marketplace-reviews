5888bfc75512601b868d46ca373cb49e2cf287ba
Darina Kormanovskaya <darina.kormanovskaya@gmail.com>
2023-02-16 00:50:26 +0300

    :boom: update parser's structure

--- parsers/ABCParser.py
+++ parsers/ABCParser.py
@@ -1,46 +1,42 @@
 import time
 from abc import ABC, abstractmethod
 from pathlib import Path
-from typing import Optional, Any, Union
+from typing import Union
 
 import pandas as pd
-from selenium import webdriver
+import undetected_chromedriver as uc
 
 
 class Parser(ABC):
     def __init__(self):
         print('Parser created!')
 
-    def __init_browser(self) -> None:
+    def _init_browser(self) -> None:
         """
         Creates Chrome webdriver.
         """
-        print("Webdriver initialization...")
-        options = webdriver.ChromeOptions()
-        options.add_argument("--disable-blink-features=AutomationControlled")
-        self.driver = webdriver.Chrome(options=options)
-        print("Webdriver started!\n")
-        self.driver.maximize_window()
-        time.sleep(1)
-
-    def _get_str_from_html(self, filename: Union[str, Path]) -> str:
-        """
-        Returns the contents of the specified html file.
-        """
-        with open(filename, "r", encoding="utf-8") as file:
-            return file.read()
+        self.driver = uc.Chrome()
+        print("Webdriver started.")
 
     @abstractmethod
-    def get_raw_html(self, url: str, filename="output.html") -> Union[str, Path]:
+    def get_raw_html(self, url: str, filename="output.html") -> None:
         """
         Creates html file with found reviews.
         """
-        self.__init_browser()
+        self._init_browser()
         print(f"Collecting data from {url} ...")
         self.driver.get(url)
         time.sleep(2)
-        return filename
 
     @abstractmethod
-    def convert_html_to_csv(self, filename) -> pd.DataFrame:
-        return pd.DataFrame()
+    def convert_html_to_csv(self, filename: Union[str, Path]) -> pd.DataFrame:
+        """
+        Convert collected html data to csv and DataFrame according to selected marketplace.
+        """
+        pass
+
+    def __del__(self):
+        try:
+            self.driver.quit()
+        except AttributeError:
+            pass
--- parsers/WildberriesParser.py
+++ parsers/WildberriesParser.py
@@ -1,17 +1,73 @@
-import pathlib
 import time
 from abc import ABC
 from datetime import datetime, timedelta
 
 import pandas as pd
-from .ABCParser import Parser
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from tqdm import tqdm
-from .utils import scroll_page
+
+from .ABCParser import Parser
+from .utils import change_html_path_to_csv, get_str_from_html, scroll_page
 
 
-class WildberriesReviewsParser(Parser, ABC):
+class WildberriesParser(Parser, ABC):
+    def get_raw_html(self, url: str, filename='output.html'):
+        super().get_raw_html(url, filename)
+        self.__go_to_reviews()
+        self.__scroll_and_save(filename)
+        print(f'Data collected and stored in \"{filename}\"')
+        self.driver.quit()
+
+    def convert_html_to_csv(self, filename) -> pd.DataFrame:
+        """
+        Convert collected comments to DataFrame and save it in {filename}.csv.
+
+        DataFrame columns:
+            | (object) author - comment author's name
+            | (object) country - their country
+            | (int64 ) rate - product rating from 1 to 5
+            | (object) text - review text
+            | (object) date - date of the comment
+            | (int64 ) upvote - likes to a comment from other users
+            | (int64 ) downvote - dislikes to a comment from other users
+            | (object) param_{i} - the i-th parameter of the product (e.g. colour)
+            | (object) photos_count - number of photos left
+            | (object) photo_{i} - link to the i-th photo
+        """
+        raw = get_str_from_html(filename)
+        soup = BeautifulSoup(raw, "lxml")
+        comments = soup.find_all(class_="comments__item")
+        print(f"Found {len(comments)} reviews")
+
+        df = pd.DataFrame()
+        for i, comment in tqdm(enumerate(comments)):
+            row = pd.DataFrame()
+            row['author'] = comment.find(class_="feedback__header").text,
+            row['country'] = comment.find(class_="feedback__country").get("class")[1][5:],
+            row['rate'] = comment.find(class_="stars-line").get("class")[2][-1],
+            row['text'] = str(comment.find(class_="feedback__text").text),
+            row['date'] = self.__convert_datetime(comment.find(class_="feedback__date").text)
+            votes = comment.find(class_="vote__wrap").find_all('span')
+            row['upvote'], row['downvote'] = votes[0].text, votes[1].text
+            for param in comment.find_all(class_="feedback__params-item"):
+                spans = param.find_all('span')
+                name = spans[0].text[:-1]
+                for i, value in enumerate(spans[1:]):
+                    row[f"param_{name}_{i}"] = value.text
+            try:
+                photos = comment.find(class_="feedback__photos").find_all('img')
+                row['photos_count'] = len(photos)
+                for idx_photo, photo in enumerate(photos):
+                    row[f"photo_{idx_photo}"] = photo.get('src')
+            except AttributeError:
+                row['photos_count'] = 0
+            df = pd.concat([df, row])
+        newname = change_html_path_to_csv(filename)
+        df.to_csv(newname, index=False)
+        print('Done! Data saved to', newname)
+        return df
+
     def __go_to_reviews(self) -> None:
         """
         Walk through the product page to get to the feedback page.
@@ -29,15 +85,8 @@ class WildberriesReviewsParser(Parser, ABC):
         with open(filename, "w", encoding="utf-8") as file:
             file.write(self.driver.page_source)
 
-    def get_raw_html(self, url: str, filename='output.html'):
-        super().get_raw_html(url, filename)
-        self.__go_to_reviews()
-        self.__scroll_and_save(filename)
-        print(f'Data collected and stored in {filename}')
-        self.driver.quit()
-        return filename
-
-    def __convert_datetime(self, raw: str) -> datetime:
+    @staticmethod
+    def __convert_datetime(raw: str) -> datetime:
         """
         Wildberries datetime format converter.
         """
@@ -59,38 +108,3 @@ class WildberriesReviewsParser(Parser, ABC):
                 date_list.append(str(datetime.now().year))
             date_list[1] = months[date_list[1]]
             return datetime.strptime(" ".join(date_list) + ', ' + times, "%d %m %Y, %H:%M")
-
-    def convert_html_to_csv(self, filename) -> pd.DataFrame:
-        print("Starting to convert")
-        raw = self._get_str_from_html(filename)
-        soup = BeautifulSoup(raw, "lxml")
-        print("Soup created")
-        comments = soup.find_all(class_="comments__item")
-        print(f"Found {len(comments)} reviews")
-
-        df = pd.DataFrame()
-        # author | country |  rate | text | date | upvote | downvote | param_... | photos_count | photo_...
-        for i, comment in tqdm(enumerate(comments)):
-            row = pd.DataFrame()
-            row['author'] = comment.find(class_="feedback__header").text,
-            row['country'] = comment.find(class_="feedback__country").get("class")[1][5:],
-            row['rate'] = comment.find(class_="stars-line").get("class")[2][-1],
-            row['text'] = str(comment.find(class_="feedback__text").text),
-            row['date'] = self.__convert_datetime(comment.find(class_="feedback__date").text)
-            votes = comment.find(class_="vote__wrap").find_all('span')
-            row['upvote'], row['downvote'] = votes[0].text, votes[1].text
-            for param in comment.find_all(class_="feedback__params-item"):
-                spans = param.find_all('span')
-                name = spans[0].text[:-1]
-                for i, value in enumerate(spans[1:]):
-                    row[f"param_{name}_{i}"] = value.text
-            photos = comment.find(class_="feedback__photos").find_all('img')
-            row['photos_count'] = len(photos)
-            for idx_photo, photo in enumerate(photos):
-                row[f"photo_{idx_photo}"] = photo.get('src')
-            df = pd.concat([df, row])
-        filename = pathlib.Path(filename)
-        newname = filename.parent / f"{filename.stem}.csv"
-        df.to_csv(newname, index=False)
-        print('Done! Data saved to', newname)
-        return df
